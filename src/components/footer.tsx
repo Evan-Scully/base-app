7156299b0acc522a64356b8c523a113a2eaf5ab0
Evan Scully <evan.scully987@gmail.com>
2023-02-16 00:40:00 +0000

    Added 404 page
    Added 404 page and externals links on index page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,12 +10,35 @@ function Navbar() {
                     <div className="py-2 px-5"><a className="text-base text-gray-500 hover:text-gray-400" href="/info/privacy-policy">Privacy Policy</a>
                     </div>
                 </nav>
-                <div className="flex justify-center space-x-6"><a href="" target="_blank" className="text-gray-400 hover:text-gray-500" rel="noreferrer">
-                    <span className="sr-only">Twitter</span></a><a href="/discord" target="_blank" className="text-gray-400 hover:text-gray-500" rel="noreferrer"><span className="sr-only">LinkedIn</span>
-                        <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="discord" className="svg-inline--fa fa-discord " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M524.5 69.84a1.5 1.5 0 0 0 -.764-.7A485.1 485.1 0 0 0 404.1 32.03a1.816 1.816 0 0 0 -1.923 .91 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.14-30.6 1.89 1.89 0 0 0 -1.924-.91A483.7 483.7 0 0 0 116.1 69.14a1.712 1.712 0 0 0 -.788 .676C39.07 183.7 18.19 294.7 28.43 404.4a2.016 2.016 0 0 0 .765 1.375A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.063-.676A348.2 348.2 0 0 0 208.1 430.4a1.86 1.86 0 0 0 -1.019-2.588 321.2 321.2 0 0 1 -45.87-21.85 1.885 1.885 0 0 1 -.185-3.126c3.082-2.309 6.166-4.711 9.109-7.137a1.819 1.819 0 0 1 1.9-.256c96.23 43.92 200.4 43.92 295.5 0a1.812 1.812 0 0 1 1.924 .233c2.944 2.426 6.027 4.851 9.132 7.16a1.884 1.884 0 0 1 -.162 3.126 301.4 301.4 0 0 1 -45.89 21.83 1.875 1.875 0 0 0 -1 2.611 391.1 391.1 0 0 0 30.01 48.81 1.864 1.864 0 0 0 2.063 .7A486 486 0 0 0 610.7 405.7a1.882 1.882 0 0 0 .765-1.352C623.7 277.6 590.9 167.5 524.5 69.84zM222.5 337.6c-28.97 0-52.84-26.59-52.84-59.24S193.1 219.1 222.5 219.1c29.67 0 53.31 26.82 52.84 59.24C275.3 310.1 251.9 337.6 222.5 337.6zm195.4 0c-28.97 0-52.84-26.59-52.84-59.24S388.4 219.1 417.9 219.1c29.67 0 53.31 26.82 52.84 59.24C470.7 310.1 447.5 337.6 417.9 337.6z"></path></svg></a>
+                <div className="flex justify-center items-center space-x-6">
+                    <a className="cursor-pointer" target="_blank" href="https://www.linkedin.com/in/evan-scully-a9893120b/">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="32"
+                            height="32"
+                            viewBox="0 0 50 50"
+                            className="reverse-svg"
+                            fill="#808080"
+                        >
+                            <path d="M25 2C12.318 2 2 12.317 2 25s10.318 23 23 23 23-10.317 23-23S37.682 2 25 2zm-7 33h-4V20h4v15zm-2-18a2 2 0 11-.001-3.999A2 2 0 0116 17zm21 18h-4v-7.5c0-1.925-1.575-3.5-3.5-3.5S26 25.575 26 27.5V35h-4V20h4v1.816A6.476 6.476 0 0130.5 20a6.5 6.5 0 016.5 6.5V35z"></path>
+                        </svg>
+                    </a>
+                    <a className="cursor-pointer" target="_blank" href="https://twitter.com/eimhin_scully">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="36"
+                            height="36"
+                            viewBox="0 0 48 48"
+                            className="reverse-svg"
+                            fill="#808080"
+                        >
+                            <path
+                                d="M42 12.429a14.978 14.978 0 01-4.247 1.162 7.38 7.38 0 003.251-4.058 14.829 14.829 0 01-4.693 1.776A7.377 7.377 0 0030.926 9c-4.08 0-7.387 3.278-7.387 7.32 0 .572.067 1.129.193 1.67a21.05 21.05 0 01-15.224-7.654 7.23 7.23 0 00-1 3.686c0 2.541 1.301 4.778 3.285 6.096a7.52 7.52 0 01-3.349-.914v.086c0 3.551 2.547 6.508 5.923 7.181a7.346 7.346 0 01-1.941.263c-.477 0-.942-.054-1.392-.135.94 2.902 3.667 5.023 6.898 5.086a14.925 14.925 0 01-9.174 3.134 14.61 14.61 0 01-1.761-.104A21.109 21.109 0 0017.321 38c13.585 0 21.017-11.156 21.017-20.834 0-.317-.01-.633-.025-.945A14.532 14.532 0 0042 12.429"
+                            ></path>
+                        </svg>
+                    </a>
                 </div>
                 <p className="text-center text-gray-400">© 2023 Evan Scully, All rights reserved.</p>
-                <p className="text-center text-gray-400"></p>
             </div>
         </footer>
     </>
